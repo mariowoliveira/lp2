7ca5df63aba77a898e8f174c9539203571713f9f
Mário W. Oliveira <mario.wilyan@hotmail.com>
2016-03-02 17:25:52 -0300

    Added files via upload
    Ultimos Exercicios

---
+++ exercicio2/Exponencial.java
@@ -0,0 +1,11 @@
+public class Exponencial{
+		
+	public static void main(String [] args){
+		System.out.println("#	Q	C");
+		for (int i=0; i<=10; i++){
+			int quadrado= i*i;
+			int cubo=i*i*i;
+			System.out.println(i+"	"+quadrado+"	"+cubo);
+		}
+	}
+}
